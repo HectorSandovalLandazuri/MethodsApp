21c312d18efbf8c84c4dcb4742456fe03eae4a3f
HectorSandovalLandazuri <88155761+HectorSandovalLandazuri@users.noreply.github.com>
2023-08-09 08:55:35 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using  Methods;
+
+SampleMethods.SayAuthor();
+SampleMethods.WelcomeUser("Héctor", "Sandoval");
+string firstName = SampleMethods.GetUserName("¿Cuál es tu nombre? ");
+string lastName = SampleMethods.GetUserName("¿Cuál es tu apellido? ");
+SampleMethods.WelcomeUser(firstName,lastName);
+(string firstName, string lastName) fullName = SampleMethods.GetFullName();
+SampleMethods.WelcomeUser(fullName.firstName,fullName.lastName);
+
+
+
+
+
+
---
+++ ConsoleUI/SampleMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+	public static class SampleMethods
+	{
+		public static void SayAuthor()
+		{
+			Console.WriteLine("*************");
+			Console.WriteLine("Writen by: Héctor Sandoval");
+			Console.WriteLine("for Mr Games");
+			Console.WriteLine("*************");
+
+		}
+
+		public static void WelcomeUser(string firstName, string lastName)
+		{
+			Console.WriteLine($"Hola {firstName} {lastName}.");
+		}
+
+		public static string GetUserName(string message)
+		{
+			Console.Write(message);
+			return Console.ReadLine();
+		}
+		//tuple allows to return several values
+		public static (string firstName,string lastName) GetFullName() {
+            Console.Write("What is your first name: ");
+			string firstName = Console.ReadLine();
+			Console.Write("What is your last name: ");
+			string lastName = Console.ReadLine();
+			return (firstName,lastName);
+		}
+
+	}
+}
